8c776b9c09395a3f1bd4ba8c3bd3f7fd8cedf816
Leonardo Rebouças <leoabcblm@gmail.com>
2025-12-25 15:19:37 -0300

    initial config e comments

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+// Carregando módulos
+const express = require('express')
+
+
+// Aplicação
+const app = express()
+
+// Import das routes
+
+// Configurações
+    // Sessão
+
+    // Middlewares
+
+    // Body Parser 
+
+    // Mongoose
+
+// Rotas
+
+// Inicialização
+
+const PORT = process.env.PORT || PORT
+
+app.listen(PORT, () => {
+    console.log("Server is running on port: " + PORT)
+})
