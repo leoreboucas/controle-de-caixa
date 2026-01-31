91cdc455bbe14019a7605f92668bff5b5dcda3db
Leonardo Rebouças <leoabcblm@gmail.com>
2026-01-31 17:35:45 -0300

    deploy config

--- backend/src/auth/firebaseAdmin.js
+++ backend/src/auth/firebaseAdmin.js
@@ -1,15 +1,24 @@
-const admin = require('firebase-admin')
-const path = require('path')
+const admin = require("firebase-admin");
+const fs = require("fs");
 
-const serviceAccount = require(path.resolve(
-    __dirname,
-    '../private_key.json'
-))
+let serviceAccount;
+
+if (process.env.NODE_ENV === "production") {
+    // Render / produção
+    serviceAccount = JSON.parse(
+        process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, "\n")
+    );
+} else {
+    // Local
+    serviceAccount = JSON.parse(
+        fs.readFileSync("src/private_key.json", "utf8")
+    );
+}
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
-    })
+        credential: admin.credential.cert(serviceAccount),
+    });
 }
 
-module.exports = admin
+module.exports = admin;
